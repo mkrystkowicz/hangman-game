fb6adf2e876ee89c1bd9d3249235f642dfaa0ede
Mateusz Krystkowicz <krissie515@gmail.com>
2020-11-09 10:58:52 +0100

    add game-info-modal feature

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
+import showGameInfo from "./showGameInfo";
 import "../scss/main.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   const gameStartBtn = document.querySelector(".game-start-btn");
   const gameInfoBtn = document.querySelector(".game-info-btn");
+
+  gameInfoBtn.addEventListener("click", showGameInfo);
 });
