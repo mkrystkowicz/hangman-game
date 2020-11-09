220eb8f351dd57ff2419350ae32cd5fe9212759b
Mateusz Krystkowicz <krissie515@gmail.com>
2020-11-09 11:55:09 +0100

    Add game start animation and game restart

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
+import gsap from "gsap";
 import showGameInfo from "./showGameInfo";
+import startGame from "./startGame";
 import "../scss/main.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   const gameStartBtn = document.querySelector(".game-start-btn");
   const gameInfoBtn = document.querySelector(".game-info-btn");
 
+  gameStartBtn.addEventListener("click", startGame);
   gameInfoBtn.addEventListener("click", showGameInfo);
 });
---
+++ src/js/startGame.js
@@ -0,0 +1,32 @@
+import gsap from "gsap";
+
+export default function startGame() {
+  scrollToGame();
+
+  const logoContainer = document.querySelector(".game__header .logo-container");
+
+  logoContainer.addEventListener("click", () => restartGame());
+}
+
+function scrollToGame() {
+  const height = window.innerHeight;
+  const header = document.querySelector(".header");
+  const gameHeader = document.querySelector(".game__header");
+
+  const tl = gsap.timeline();
+  tl.to(header, { duration: 0.5, scale: 0, y: `-=${height / 4}` });
+  window.scrollTo({
+    top: height,
+    left: 0,
+    behavior: "smooth",
+  });
+  tl.fromTo(
+    gameHeader,
+    { opacity: 0, y: "-=100" },
+    { duration: 0.3, opacity: 1, y: 0 }
+  );
+}
+
+function restartGame() {
+  window.location.assign(window.location.href);
+}
