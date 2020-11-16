1757c0c4737a6eacf67fc0753ffdf20948628eec
Mateusz Krystkowicz <krissie515@gmail.com>
2020-11-16 19:28:18 +0100

    Add end modal animations

--- src/js/endGame.js
+++ src/js/endGame.js
@@ -1,20 +1,30 @@
+import gsap from "gsap";
+
 export default function endGame(result) {
   const gameEndModal = document.querySelector(".end-game-modal");
   const title = gameEndModal.querySelector(".end-game-modal__title");
   const subTitle = gameEndModal.querySelector(".end-game-modal__subtitle");
   const button = gameEndModal.querySelector(".end-game-modal__button");
 
-  gameEndModal.classList.add("end-game-modal--active");
-
-  if (result === "win") {
-    title.textContent = "you won";
-    subTitle.textContent = "unfortunately we don't have any awards 😥";
-  } else {
-    title.textContent = "game over";
-    subTitle.textContent = "don't worry you can try again 😅";
-  }
-
-  button.addEventListener("click", () =>
+    gameEndModal.classList.add("end-game-modal--active");
+    
+    
+    if (result === "win") {
+        title.textContent = "you won";
+        subTitle.textContent = "unfortunately we don't have any awards 😥";
+    } else {
+        title.textContent = "game over";
+        subTitle.textContent = "don't worry you can try again 😅";
+    }
+    
+    gameEndModal.addEventListener('transitionend', () => animateModalElements(title, subTitle, button))
+    
+    button.addEventListener("click", () =>
     window.location.assign(window.location.href)
   );
+}
+
+function animateModalElements(title, subTitle, button) {
+    gsap.fromTo([title,subTitle], {opacity: 0, y: '-100'}, {duration: .5, opacity: 1, y:0})
+    gsap.fromTo(button, {opacity: 0, y: '+100'}, {duration: .5, opacity: 1, y:0})
 }
