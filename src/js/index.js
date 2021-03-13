19d03f7b2a81b1d5f417cfb330c01d5e8c44cdda
Mateusz <krissie515@gmail.com>
2021-03-13 15:57:05 +0100

    refactor in getWordDefinition async function

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
-import showGameInfo from "./showGameInfo";
-import startGame from "./startGame";
-import "../scss/main.scss";
+import showGameInfo from './showGameInfo';
+import startGame from './startGame';
+import '../scss/main.scss';
 
-window.addEventListener("DOMContentLoaded", () => {
-  const gameStartBtn = document.querySelector(".game-start-btn");
-  const gameInfoBtn = document.querySelector(".game-info-btn");
-  
-  window.addEventListener("beforeunload", () => {
+window.addEventListener('DOMContentLoaded', () => {
+  const gameStartBtn = document.querySelector('.game-start-btn');
+  const gameInfoBtn = document.querySelector('.game-info-btn');
+
+  window.addEventListener('beforeunload', () => {
     window.scrollTo(0, 0);
   });
 
-  gameStartBtn.addEventListener("click", startGame);
-  gameInfoBtn.addEventListener("click", showGameInfo);
+  gameStartBtn.addEventListener('click', startGame);
+  gameInfoBtn.addEventListener('click', showGameInfo);
 });
